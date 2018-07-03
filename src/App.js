3e485942b86291fad5916d241beaa15e63bf3e03
Tejaswini515 <tejaswinikosunam@gmail.com>
2018-07-03 17:33:38 -0500

    Search Users

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import Fetch from './Fetch';
+import './App.css';
+
+class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-headerTitle">Github Search</h1>
+        </header>
+        <main className="App-main">
+          <h3 className="App-title">Github-Search</h3>
+          <Fetch/>
+        </main>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Fetch.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class Fetch extends Component {
+
+    state = {
+        userName: '',
+        list: null
+    }
+
+    diffUser = async(e) => {
+        e.preventDefault();
+        await this.setState({
+            userName: e.target.value,
+        });
+        this.handleFetch();
+    }
+
+    handleFetch = () => {
+        axios.get(`https://api.github.com/users/${this.state.userName}/repos`)
+        .then(res => {
+            if(res.status === 404) {
+                console.log('No such user')
+            }
+            else {
+                if(res.data.length === 0){
+                    this.setState({list: `${this.state.userName} does not have any repos`})
+                }
+                else {
+                    const list = res.data.map(repo => {
+                        return <li style={{listStyle : "none"}} key={repo["id"]}>
+                                    <strong>{repo["name"]}</strong>   
+                                    <a style={{float:"right"}} href={repo["html_url"]}>{repo["html_url"]}</a>
+                                </li>
+                    })
+                    this.setState({list: list});
+                }
+
+            }
+        }).catch(err => {
+                        console.log('Username does not exists')
+                        this.setState({list: 'No Such User'});
+            });
+    } 
+
+    render(){
+        
+        return(
+            <div>
+                <input style={{width: "100%",height:"25px",borderTop:"2px"}} 
+                       type="text" 
+                       onChange={this.diffUser}
+                       placeHolder="Please enter username"/>
+                <ul style={{padding:'0px'}}> {this.state.list} </ul>
+            </div>
+        );
+    }
+}
+
+export default Fetch;
