3d80c203c48260b1f8029b880725424e2ed76884
Tejaswini515 <tejaswinikosunam@gmail.com>
2018-07-06 21:43:42 -0500

    Added Message

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -7,7 +7,7 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
     switch(action.type) {
-        case "USER_FOUND": return Object.assign({}, state, {list: action.data});
+        case "USER_FOUND": return Object.assign({}, state, {list: action.data, message: ''});
 
         case "USER_NOT_FOUND": return Object.assign({}, state, {list: [], message: 'No Such User'});
 
